a9706552419c708d8f12c581ac36040d828263dd
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-06-08 17:19:33 +0900

    feat: Add video capture

--- examples/movenet_pose_estimation.py
+++ examples/movenet_pose_estimation.py
@@ -37,46 +37,112 @@ from PIL import Image
 from PIL import ImageDraw
 from pycoral.adapters import common
 from pycoral.utils.edgetpu import make_interpreter
+import cv2
+import numpy as np
+import tensorflow as tf
 
 _NUM_KEYPOINTS = 17
 
+EDGES = {
+    (0, 1): "m",
+    (0, 2): "c",
+    (1, 3): "m",
+    (2, 4): "c",
+    (0, 5): "m",
+    (0, 6): "c",
+    (5, 7): "m",
+    (7, 9): "m",
+    (6, 8): "c",
+    (8, 10): "c",
+    (5, 6): "y",
+    (5, 11): "m",
+    (6, 12): "c",
+    (11, 12): "y",
+    (11, 13): "m",
+    (13, 15): "m",
+    (12, 14): "c",
+    (14, 16): "c",
+}
+
+
+def draw_keypoints(frame, keypoints, confidence_threshold):
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints, [y, x, 1]))
+
+    for kp in shaped:
+        ky, kx, kp_conf = kp
+        if kp_conf > confidence_threshold:
+            cv2.circle(frame, (int(kx), int(ky)), 4, (0, 255, 0), -1)
+
+
+def draw_connections(frame, keypoints, edges, confidence_threshold):
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints, [y, x, 1]))
+
+    for edge, color in edges.items():
+        p1, p2 = edge
+        y1, x1, c1 = shaped[p1]
+        y2, x2, c2 = shaped[p2]
+
+        if (c1 > confidence_threshold) & (c2 > confidence_threshold):
+            cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+
 
 def main():
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument(
-      '-m', '--model', required=True, help='File path of .tflite file.')
-  parser.add_argument(
-      '-i', '--input', required=True, help='Image to be classified.')
-  parser.add_argument(
-      '--output',
-      default='movenet_result.jpg',
-      help='File path of the output image.')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="File path of .tflite file."
+    )
+    args = parser.parse_args()
 
-  interpreter = make_interpreter(args.model)
-  interpreter.allocate_tensors()
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
 
-  img = Image.open(args.input)
-  resized_img = img.resize(common.input_size(interpreter), Image.ANTIALIAS)
-  common.set_input(interpreter, resized_img)
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-  interpreter.invoke()
+        # Reshape image
+        img = frame.copy()
+        resized_img = img.resize(common.input_size(interpreter), Image.ANTIALIAS)
+        common.set_input(interpreter, resized_img)
+        output_details = interpreter.get_output_details()
+        interpreter.invoke()
+        keypoints_with_scores = interpreter.get_tensor(output_details[0]["index"])
 
-  pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
-  print(pose)
-  draw = ImageDraw.Draw(img)
-  width, height = img.size
-  for i in range(0, _NUM_KEYPOINTS):
-    draw.ellipse(
-        xy=[
-            pose[i][1] * width - 2, pose[i][0] * height - 2,
-            pose[i][1] * width + 2, pose[i][0] * height + 2
-        ],
-        fill=(255, 0, 0))
-  img.save(args.output)
-  print('Done. Results saved at', args.output)
+        pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
+
+        # Rendering
+        draw_connections(img, keypoints_with_scores, EDGES, 0.4)
+        draw_keypoints(img, keypoints_with_scores, 0.4)
+
+        cv2.imshow("MoveNet Lightning", frame)
+
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    # pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
+    # print(pose)
+    # draw = ImageDraw.Draw(img)
+    # width, height = img.size
+    # for i in range(0, _NUM_KEYPOINTS):
+    #     draw.ellipse(
+    #         xy=[
+    #             pose[i][1] * width - 2,
+    #             pose[i][0] * height - 2,
+    #             pose[i][1] * width + 2,
+    #             pose[i][0] * height + 2,
+    #         ],
+    #         fill=(255, 0, 0),
+    #     )
+    # img.save(args.output)
+    # print("Done. Results saved at", args.output)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
